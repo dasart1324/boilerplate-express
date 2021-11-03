69a6daa1291f3f031c5ca49e81e1df52032fa878
dasart1324 <79549741+dasart1324@users.noreply.github.com>
2021-11-03 14:55:40 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -5,6 +5,12 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
+
 
 
 
