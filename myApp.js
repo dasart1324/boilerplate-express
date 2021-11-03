749c15d32882bd56db2b0592900d13ac0747f12e
dasart1324 <79549741+dasart1324@users.noreply.github.com>
2021-11-03 17:12:51 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -6,9 +6,7 @@ app.get("/", function(req, res) {
 });
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json"
-  });
+  res.json({"message": process.env.MESSAGE_STYLE == "uppercase" ? "HELLO JSON" : "Hello json"})
 });
 
 
